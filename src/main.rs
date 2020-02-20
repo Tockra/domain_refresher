226ff46dcd4f5a8329a7d42eaa02a8b4b77ddb2c
Tim <tim.tannert@tu-dortmund.de>
2020-02-20 21:06:15 +0100

    built skelleton

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,38 @@
 use serde_json::Value;
-fn main() {
-    println!("Hello world");
+
+enum InternalError {
+    Config,
+    URL,
+    LOGIN
 }
 
+fn main() {
 
-fn check_url(url: &str) -> Result<(),String> {
+    // gets the json from the config and checks the format of the config.json
+
+    // start to check frequently the given domains. Returns if something runs wrong
+
+}
+
+fn frequently_check(interval: u32,domains: Vec<String>) -> Result<(),InternalError> {
+    unimplemented!();
+}
+
+fn parse_config(file_path: &str) -> Result<Value,InternalError> {
+    unimplemented!();
+}
+
+fn get_own_ip() -> String {
+    unimplemented!();
+}
+
+fn get_current_ip(domain: &str) -> String {
+    unimplemented!();
+}
+
+fn check_url(url: &str) -> Result<(),InternalError> {
     if url.contains(",") || url.contains(":") || url.contains("/") {
-        return Err("Falsches Format der URL");
+        return Err(InternalError::URL);
     } 
 
     Ok(())
