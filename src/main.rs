f3605e321dc15b2b516bd09adfd5e26b1c03c2b8
Tim <tim.tannert@tu-dortmund.de>
2020-02-20 20:55:01 +0100

    added serde

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use serde_json::Value;
 fn main() {
-    println!("Hello, world!");
+    println!("Hello world");
+}
+
+
+fn check_url(url: &str) -> Result<(),String> {
+    if url.contains(",") || url.contains(":") || url.contains("/") {
+        return Err("Falsches Format der URL");
+    } 
+
+    Ok(())
 }
